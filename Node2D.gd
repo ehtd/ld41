02879bc156b9a072c8ae5564d474314692c80bad
Ernesto Torres <ernesto.torres.d@gmail.com>
2018-04-21 17:27:17 -0500

    Adding typable label

---
+++ Node2D.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+var original = ""
+var selected = true
+
+func generateWord():
+	return "word"
+	
+func _ready():
+	original = generateWord()
+	$Label.text = original
+
+func tryToRemove(letter):
+	prints("Current word ", original)
+	if original.empty():
+		return
+		
+	if original.begins_with(letter):
+		prints("begins")
+		original.erase(0, 1)
+		$Label.text = original
+		
+		if original.empty():
+			prints("destroying")
+	
+		
+func _input(event):
+	if selected == false:
+		return
+		
+	if event is InputEventKey:
+		if event.is_echo():
+			return
+			
+		if event.pressed:
+			var key = OS.get_scancode_string(event.scancode).to_lower()
+			prints("Key pressed", key)
+			tryToRemove(key)
